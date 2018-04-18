a42d6802d6bac376cfb039a7acfaf25779e681c6
yanz <dirtysalt1987@gmail.com>
2018-04-18 18:33:57 +0800

    x

--- implicit/__init__.py
+++ implicit/__init__.py
@@ -1,6 +1,6 @@
-from . import als, approximate_als, nearest_neighbours
+from . import als, approximate_als, bpr, nearest_neighbours
 from .als import alternating_least_squares
 
 __version__ = '0.3.4'
 
-__all__ = [alternating_least_squares, approximate_als, als, nearest_neighbours, __version__]
+__all__ = [alternating_least_squares, bpr, approximate_als, als, nearest_neighbours, __version__]
